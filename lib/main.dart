f12c2080075fa94826ea288d250d3bd415d7cdab
Mir  Mosarof Hossan <showravofficial@gmail.com>
2023-04-12 23:11:29 +0600

    commit Second

--- lib/main.dart
+++ lib/main.dart
@@ -21,49 +21,51 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/images/Login.png'),
-              SizedBox(height: 16.0),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  prefixIcon: Icon(Icons.email),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/images/Login.png'),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    prefixIcon: Icon(Icons.email),
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  prefixIcon: Icon(Icons.lock),
+                SizedBox(height: 16.0),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(Icons.lock),
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.0),
-              TextButton(
-                onPressed: () {
-                  // Perform forgot password logic here
-                },
-                child: Text('Forgot Password?'),
-              ),
-              SizedBox(height: 24.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Perform login logic here
-                },
-                child: Text('Login'),
-              ),
-              TextButton(
-                onPressed: () {
-                  // Navigate to signup page
-                },
-                child: Text('Sign up'),
-              ),
-            ],
+                SizedBox(height: 16.0),
+                TextButton(
+                  onPressed: () {
+                    // Perform forgot password logic here
+                  },
+                  child: Text('Forgot Password?'),
+                ),
+                SizedBox(height: 24.0),
+                ElevatedButton(
+                  onPressed: () {
+                    // Perform login logic here
+                  },
+                  child: Text('Login'),
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Navigate to signup page
+                  },
+                  child: Text('Sign up'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
